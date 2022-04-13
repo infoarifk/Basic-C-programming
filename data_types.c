943aac9c5cfb191e3627fa7641a9ba5026bbe292
Md Arif Khan <101652388+md-arifkhan@users.noreply.github.com>
2022-04-13 21:07:39 +0600

    Add files via upload

---
+++ data_types.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a=30; //int data
+
+    float b=9.81; //float data
+
+    double c=3.141687; //double data
+
+    char d='A'; //char data
+
+    printf(" output of these values:\n %d\n% .2f\n %lf\n %c",a,b,c,d); //"\n" for new line
+
+    return 0;
+}
